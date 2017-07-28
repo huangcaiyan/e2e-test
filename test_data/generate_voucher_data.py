6a761ee199801f806ab2b6ffc6539ac4c12398ec
codingsheepgirl <18514509382@163.com>
2017-07-28 16:20:55 +0800

    tt

---
+++ test_data/generate_voucher_data.py
@@ -0,0 +1,21 @@
+#生成凭证测试数据
+
+RecordIncomePublicData = [
+    ['1','现金','内部代表']
+]
+
+RecordIncomeItemsData = [
+    ['1',['1','1'],'111','现金-内部代表-利息收入-利息收入 111']
+]
+
+RecordOutcomePublicData = [
+    ['1','现金','内部代表']
+]
+
+RecordOutputItemsData = [
+    ['1',['1','1'],'211','现金-内部代表-行政支出-银行费用 211']
+]
+
+RecordTransferData = [
+    ['1','招商银行','现金','711','招商银行-现金 711']
+]
